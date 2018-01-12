2eef2fac2d34eb50c97587ddcdfe169767c52f1d
Witold Olszyk <witoldolszyk@gmail.com>
2018-01-12 22:47:30 +0100

    czarny

---
+++ script/scripts.js
@@ -0,0 +1,12 @@
+$(function()	{
+	var span = $("span");
+	span.each(function(index, element) {
+		if(index % 2 == 0) {
+			$(element).css('color', 'red');
+		};
+	})
+	var paragraphs = $('p');
+	paragraphs.each(function(index, element) {
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    $(element).append(button) });
+;})
