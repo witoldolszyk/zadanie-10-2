b81314da6e210b578b2e92d042966018709aa782
Witold Olszyk <witoldolszyk@gmail.com>
2018-01-12 22:53:39 +0100

    czarny

--- script/scripts.js
+++ script/scripts.js
@@ -9,4 +9,7 @@ $(function()	{
 	paragraphs.each(function(index, element) {
     var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
     $(element).append(button) });
+		$("button").click(function(){
+			alert($(this).attr("data-tmp"));
+});
 ;})
